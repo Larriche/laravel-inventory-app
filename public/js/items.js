f9b823166c832be8e49edc5c1ee77f3a1b7f88bb
Richman Clifford <larriche99@gmail.com>
2017-06-06 14:24:29 -0700

    Worked on delete functionality of items

--- public/js/items.js
+++ public/js/items.js
@@ -61,6 +61,20 @@ var Items = {
 			e.preventDefault();
 			App.submitForm(this, Items.refreshItems, $('#items-update-errors-container'));
 		});
+
+		// Event handler for click of item delete button
+		$(document).on('click', '.delete-item', function(){
+			var id = $(this).data('id');
+            App.setDeleteForm('/items/' + id, 'item-delete-form', 'Delete Item From Inventory');
+            App.showConfirmDialog("Do you want to delete this item?");
+		});
+
+		// Event handler for when form for deleting item is submitted
+		$(document).on('submit', '#item-delete-form', function(e){
+			e.preventDefault();
+			App.submitForm(this, Items.refreshItems, null);
+			App.hideConfirmDialog();
+		});
 	}
 };
 
